569a4598b6a6defbb742e37c3f318ca411e7b9b7
r321432 <dragonryan06@gmail.com>
2021-08-27 08:03:51 -0400

    Added random direction movement and activity code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from classes import *
+from random import randint
 pygame.init()
 
 grid = {} # format: {cell:obj, cell2:obj2}
@@ -11,21 +12,26 @@ def create_particle(particle): # creates a grid entry for the new particle
     if particle_types[particle.type]['move_type'] != 'static':
         particle.active = True
 
-def move_particle(particle): # i believe its possible particles could move twice if they were displaced by another particle falling and then they moved, this probably can be fixed with a "moved" bool
-    neighbors = {
+def move_particle(particle): # i believe its possible particles could move twice if they were displaced by another particle falling and then they moved, this probably can be fixed with a "moved" bool property that says if the particle moved that frame already
+    moved = False
+    direction = randint(0,1)
+    if direction == 0:
+        direction = -1
+    neighbors = { # left/right is relative to which dir was picked to go first by the randint
         'down' : [particle.pos[0],particle.pos[1]+1],
-        'downright' : [particle.pos[0]+1,particle.pos[1]+1],
-        'downleft' : [particle.pos[0]-1,particle.pos[1]+1],
-        'right' : [particle.pos[0]+1,particle.pos[1]],
-        'left' : [particle.pos[0]-1,particle.pos[1]],
+        'downdiagonal1' : [particle.pos[0]+direction,particle.pos[1]+1],
+        'downdiagonal2' : [particle.pos[0]-direction,particle.pos[1]+1],
+        'side1' : [particle.pos[0]+direction,particle.pos[1]],
+        'side2' : [particle.pos[0]-direction,particle.pos[1]],
         'up' : [particle.pos[0],particle.pos[1]-1],
-        'upright' : [particle.pos[0]+1,particle.pos[1]-1],
-        'upleft' : [particle.pos[0]-1,particle.pos[1]-1]}
+        'updiagonal1' : [particle.pos[0]+direction,particle.pos[1]-1],
+        'updiagonal2' : [particle.pos[0]-direction,particle.pos[1]-1]}
 
     if not str(neighbors['down']) in grid.keys() and particle_types[particle.type]['density'] > 0:
         del grid[str(particle.pos)]
         grid[str(neighbors['down'])] = particle
         particle.pos = neighbors['down']
+        moved = True
     elif str(neighbors['down']) in grid.keys() and particle_types[grid[str(neighbors['down'])].type]['density'] < particle_types[particle.type]['density']:
         del grid[str(particle.pos)]
         replacing_particle = grid[str(neighbors['down'])]
@@ -34,81 +40,102 @@ def move_particle(particle): # i believe its possible particles could move twice
         grid[str(neighbors['down'])] = particle
         replacing_particle.pos = particle.pos
         particle.pos = neighbors['down']
+        moved = True
 
-    elif not str(neighbors['downright']) in grid.keys() and particle_types[particle.type]['density'] > 0:
+    elif not str(neighbors['downdiagonal1']) in grid.keys() and particle_types[particle.type]['density'] > 0:
         del grid[str(particle.pos)]
-        grid[str(neighbors['downright'])] = particle
-        particle.pos = neighbors['downright']
-    elif str(neighbors['downright']) in grid.keys() and particle_types[grid[str(neighbors['downright'])].type]['density'] < particle_types[particle.type]['density']:
+        grid[str(neighbors['downdiagonal1'])] = particle
+        particle.pos = neighbors['downdiagonal1']
+        moved = True
+    elif str(neighbors['downdiagonal1']) in grid.keys() and particle_types[grid[str(neighbors['downdiagonal1'])].type]['density'] < particle_types[particle.type]['density']:
         del grid[str(particle.pos)]
-        replacing_particle = grid[str(neighbors['downright'])]
-        del grid[str(neighbors['downright'])]
+        replacing_particle = grid[str(neighbors['downdiagonal1'])]
+        del grid[str(neighbors['downdiagonal1'])]
         grid[str(particle.pos)] = replacing_particle
-        grid[str(neighbors['downright'])] = particle
+        grid[str(neighbors['downdiagonal1'])] = particle
         replacing_particle.pos = particle.pos
-        particle.pos = neighbors['downright']
+        particle.pos = neighbors['downdiagonal1']
+        moved = True
 
-    elif not str(neighbors['downleft']) in grid.keys() and particle_types[particle.type]['density'] > 0:
+    elif not str(neighbors['downdiagonal2']) in grid.keys() and particle_types[particle.type]['density'] > 0:
         del grid[str(particle.pos)]
-        grid[str(neighbors['downleft'])] = particle
-        particle.pos = neighbors['downleft']
-    elif str(neighbors['downleft']) in grid.keys() and particle_types[grid[str(neighbors['downleft'])].type]['density'] < particle_types[particle.type]['density']:
+        grid[str(neighbors['downdiagonal2'])] = particle
+        particle.pos = neighbors['downdiagonal2']
+        moved = True
+    elif str(neighbors['downdiagonal2']) in grid.keys() and particle_types[grid[str(neighbors['downdiagonal2'])].type]['density'] < particle_types[particle.type]['density']:
         del grid[str(particle.pos)]
-        replacing_particle = grid[str(neighbors['downleft'])]
-        del grid[str(neighbors['downleft'])]
+        replacing_particle = grid[str(neighbors['downdiagonal2'])]
+        del grid[str(neighbors['downdiagonal2'])]
         grid[str(particle.pos)] = replacing_particle
-        grid[str(neighbors['downleft'])] = particle
+        grid[str(neighbors['downdiagonal2'])] = particle
         replacing_particle.pos = particle.pos
-        particle.pos = neighbors['downleft']
+        particle.pos = neighbors['downdiagonal2']
+        moved = True
 
     # there is no need for having things bubble up as the substance the are in will push them up
     elif not str(neighbors['up']) in grid.keys() and particle_types[particle.type]['density'] < 0:
         del grid[str(particle.pos)]
         grid[str(neighbors['up'])] = particle
         particle.pos = neighbors['up']
+        moved = True
     
-    elif not str(neighbors['upright']) in grid.keys() and particle_types[particle.type]['density'] < 0:
+    elif not str(neighbors['updiagonal1']) in grid.keys() and particle_types[particle.type]['density'] < 0:
         del grid[str(particle.pos)]
-        grid[str(neighbors['upright'])] = particle
-        particle.pos = neighbors['upright']
+        grid[str(neighbors['updiagonal1'])] = particle
+        particle.pos = neighbors['updiagonal1']
+        moved = True
     
-    elif not str(neighbors['upleft']) in grid.keys() and particle_types[particle.type]['density'] < 0:
+    elif not str(neighbors['updiagonal2']) in grid.keys() and particle_types[particle.type]['density'] < 0:
         del grid[str(particle.pos)]
-        grid[str(neighbors['upleft'])] = particle
-        particle.pos = neighbors['upleft']
+        grid[str(neighbors['updiagonal2'])] = particle
+        particle.pos = neighbors['updiagonal2']
+        moved = True
 
     elif particle_types[particle.type]['move_type'] == 'fluid':
-        if not str(neighbors['right']) in grid.keys():
+        if not str(neighbors['side1']) in grid.keys():
             del grid[str(particle.pos)]
-            grid[str(neighbors['right'])] = particle
-            particle.pos = neighbors['right']
-        elif str(neighbors['right']) in grid.keys() and particle_types[grid[str(neighbors['right'])].type]['density'] < particle_types[particle.type]['density']:
+            grid[str(neighbors['side1'])] = particle
+            particle.pos = neighbors['side1']
+            moved = True
+        elif str(neighbors['side1']) in grid.keys() and particle_types[grid[str(neighbors['side1'])].type]['density'] < particle_types[particle.type]['density']:
             del grid[str(particle.pos)]
-            replacing_particle = grid[str(neighbors['right'])]
-            del grid[str(neighbors['right'])]
+            replacing_particle = grid[str(neighbors['side1'])]
+            del grid[str(neighbors['side1'])]
             grid[str(particle.pos)] = replacing_particle
-            grid[str(neighbors['right'])] = particle
+            grid[str(neighbors['side1'])] = particle
             replacing_particle.pos = particle.pos
-            particle.pos = neighbors['right']
+            particle.pos = neighbors['side1']
+            moved = True
 
-        elif not str(neighbors['left']) in grid.keys():
+        elif not str(neighbors['side2']) in grid.keys():
             del grid[str(particle.pos)]
-            grid[str(neighbors['left'])] = particle
-            particle.pos = neighbors['left']
-        elif str(neighbors['left']) in grid.keys() and particle_types[grid[str(neighbors['left'])].type]['density'] < particle_types[particle.type]['density']:
+            grid[str(neighbors['side2'])] = particle
+            particle.pos = neighbors['side2']
+            moved = True
+        elif str(neighbors['side2']) in grid.keys() and particle_types[grid[str(neighbors['side2'])].type]['density'] < particle_types[particle.type]['density']:
             del grid[str(particle.pos)]
-            replacing_particle = grid[str(neighbors['left'])]
-            del grid[str(neighbors['left'])]
+            replacing_particle = grid[str(neighbors['side2'])]
+            del grid[str(neighbors['side2'])]
             grid[str(particle.pos)] = replacing_particle
-            grid[str(neighbors['left'])] = particle
+            grid[str(neighbors['side2'])] = particle
             replacing_particle.pos = particle.pos 
-            particle.pos = neighbors['left']
+            particle.pos = neighbors['side2']
+            moved = True
+    return moved, neighbors
 
 def update_world():
     particles = list(grid.values())
     for p in particles:
         if p.active:
-            move_particle(p)
+            moved, neighbors = move_particle(p)
+            if moved == False:
+                p.active = False
+            else:
+                for n in neighbors.values():
+                    if str(n) in grid.keys():
+                        if particle_types[grid[str(n)].type]['move_type'] != 'static':
+                            grid[str(n)].active = True
+            
         pygame.draw.rect(constants.DISPLAY,particle_types[p.type]['color'],(p.pos[0]*constants.CELLSIZE,p.pos[1]*constants.CELLSIZE,constants.CELLSIZE,constants.CELLSIZE))
 
 def handle_input(event):
